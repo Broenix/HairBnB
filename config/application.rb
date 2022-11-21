2884396e8538fbd3b81a3328809d35fd46391931
theo chevalier <chevalie.theo@gmail.com>
2022-11-21 16:51:53 +0100

    update config/application.rb 4 segmented error. look4 BMO

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,11 @@ module HairBnB
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # BMO TEST
+    config.assets.configure do |env|
+      env.export_concurrent = false
+    end
+    # END BMO TEST
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
