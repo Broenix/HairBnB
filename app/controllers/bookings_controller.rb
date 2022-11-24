d4fca547ca5c84c03e79db0fc7e0bb4e8cbba425
Chloe Lagneaux <chloelagneaux2@gmail.com>
2022-11-24 11:57:31 +0100

    create booking done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,24 @@ class BookingsController < ApplicationController
     @bookings = Booking.all
   end
 
-  def show
-    @bookings = Wig.find(params[:id])
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.wig_id = params[:wig_id]
+    @booking.user_id = current_user.id
+    
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(
+      :start_date,
+      :end_date
+    )
   end
 end
