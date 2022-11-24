6fc6710c3b211502c33d47e5624d0e4170997098
Paul Barbaud <paolbarbaud@gmail.com>
2022-11-24 15:37:17 +0100

    ça va être légendaire

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,8 @@
 class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
+    @bookings_renter = current_user.booking_as_a_renter
+    @bookings_owner = current_user.booking_as_a_owner
   end
 
   def show
